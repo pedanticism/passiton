ae8f6c4949421c84510faa53404ba02e1034b9a3
Stephen Connolly <14976148+stephenconnolly1@users.noreply.github.com>
2021-01-16 13:49:52 +0000

    Update update.sh
    Added Chrome download and install

--- update.sh
+++ update.sh
@@ -8,8 +8,12 @@ sudo apt upgrade
 # Download zoom and Teams  installer
 wget -O Downloads/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
 wget -O Downloads/ms-teams.deb https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.958_amd64.deb
-cd Downloads/
+wget -O Downloads/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
 
 # Install Zoom and teams
 sudo dpkg -i zoom.deb
 sudo dpkg -i ms-teams.deb
+sudo dpkg -i chrome.deb
+
+
