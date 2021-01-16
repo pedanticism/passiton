d880851a85d9d4f753cf99ff82da641d9fd4492a
Stephen Connolly <stephen@pedanticism.net>
2021-01-16 17:45:30 +0000

    Add  ability resolve dependencies

--- update.sh
+++ update.sh
@@ -6,14 +6,15 @@ sudo apt update
 sudo apt upgrade
 
 # Download zoom and Teams  installer
-wget -O Downloads/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
-wget -O Downloads/ms-teams.deb https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.958_amd64.deb
-wget -O Downloads/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget -O ~/Downloads/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+wget -O ~/Downloads/ms-teams.deb https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.958_amd64.deb
+wget -O ~/Downloads/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+cd ~/Downloads
 
 # Install Zoom and teams
-sudo dpkg -i zoom.deb
-sudo dpkg -i ms-teams.deb
-sudo dpkg -i chrome.deb
+sudo apt-get --assume-yes install ./zoom.deb
+sudo apt-get --assume-yes install ./ms-teams.deb
+sudo apt-get --assume-yes install ./chrome.deb
 
 
